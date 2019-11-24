58dd823d467f899f2167c2a94d082c6236e61c02
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-11-24 18:24:23 +0100

    StringifyJSON and getElementsByClass solved

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+console.clear()
+var getElementsByClassName = function(className, elemnt) {
+    elemnt = elemnt || document.body;
+    var result = [];
+
+    if(elemnt.classList && Array.from(elemnt.classList).includes(className)){
+        result.push(elemnt);
+    }
+
+    Array.from(elemnt.childNodes).forEach(function(node) {
+        result = result.concat( getElementsByClassName(className, node));
+    });
+
+    return result;
+
 };
+// console.log(getElementsByClassName('target'))
